327f1f97141357feb1461550fa352d56fa45bf92
webdeveloperNP <nishant24patel@gmail.com>
2023-07-17 16:18:06 +0530

    Name correction todo->TextUtils

--- newsapp/src/components/News.jsx
+++ newsapp/src/components/News.jsx
@@ -8,7 +8,7 @@ export default class News extends Component {
         <h2>Top Headlines</h2>
         <div className="row">
             <div className="col-md-3 mx-4">
-                <NewsItem title='My title' description='This is a news card'/>
+                <NewsItem title='My title' description='This is a news card' url="https://s.yimg.com/ny/api/res/1.2/qxqwDKvbkFRLdZQOvRWPcA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04MDA-/https://s.yimg.com/os/creatr-uploaded-images/2023-07/92bc72b0-237a-11ee-bbbf-426f5a5c945f" class="card-img-top" alt="Image Here" />
             </div>
             <div className="col-md-3 mx-4">
                 <NewsItem title='My title' description='This is a news card'/>
